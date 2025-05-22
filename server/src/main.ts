15aa56f2c20a79af3697b70bd1b5facf3fa1b97a
leo <leo006401@gmail.com>
2025-05-22 17:53:55 +0530

    railway deploy ready

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,11 +7,8 @@ import * as cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
+import { hostname } from 'os';
 
-// import * as mongoose from 'mongoose';
-
-
-const PORT = 1000;
 const SESSION_TIME = 30 * 60 * 1000;
 
 async function bootstrap() {
@@ -29,10 +26,9 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('apidocs', app, document);
-    const configService = app.get(ConfigService);
-
 
   app.use(cookieParser());
+  const configService = app.get(ConfigService);
 
   app.use(
     session({
@@ -50,18 +46,19 @@ async function bootstrap() {
       },
     }),
   );
-  
+
   app.enableCors({
     origin: configService.get<string>('FRONTEND_URL'),
     credentials: true,
   });
 
-   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads',
   });
 
-  await app.listen(PORT, () =>
-    console.log(`Server Listening on ${PORT} port. `),
-  );
+  const port = configService.get<string>('PORT') || 1500
+  await app.listen(port, '0.0.0.0', () => {
+    console.log(`Server Listening on port ${port}`);
+  });
 }
 bootstrap();
