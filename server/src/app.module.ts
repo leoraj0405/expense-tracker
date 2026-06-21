0f2907b775a390aacd72bfc1862b5dfe27f9ae27
leoraj0405 <leo006401@gmail.com>
2026-06-21 19:44:13 +0530

    db config changed

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -26,13 +26,17 @@ import { JwtModule } from '@nestjs/jwt';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'mysql',
-        host: config.get<string>('DB_HOST', 'localhost'),
-        port: config.get<number>('DB_PORT', 3306),
-        username: config.get<string>('DB_USER', 'root'),
-        password: config.get<string>('DB_PASSWORD', 'Root1234@'),
-        database: config.get<string>('DB_NAME', 'expensestracker'),
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
         autoLoadEntities: true,
         synchronize: true,
+        ssl: {
+          minVersion: 'TLSv1.2',
+          rejectUnauthorized: true,
+        },
       }),
     }),
     JwtModule.registerAsync({
